31bf4665630813899589bdda810ef7d5c370f83d
lawalenoch23-web <lawalenoch23@gmail.com>
2026-04-22 12:42:29 +0100

    Update supabaseClient.ts

--- client/src/supabaseClient.ts
+++ client/src/supabaseClient.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// These are like your digital keys.
-// You get these for free when you sign up at supabase.com
-const supabaseUrl = "https://tqlpgeqnbopaomonhrme.supabase.co";
-const supabaseAnonKey = "sb_publishable_EdFTM_8qcaf8PoOg-DbZzg_vZ198AVM";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
